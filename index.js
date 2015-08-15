ec25a1a9530062b3c06bdf1b373f87b5133a78af
Georgette Pincin <georgette@pincin.com>
2015-08-15 13:55:01 -0400

    make it so

---
+++ index.js
@@ -0,0 +1,11 @@
+
+$( document ).ready(function() {
+//     var rect = Snap().rect(0, 0, 10, 10);
+// Snap.animate(0, 10, function (val) {
+//     rect.attr({
+//         x: val
+//     });
+// }, 1000);
+// // in given context is equivalent to
+// rect.animate({x: 10}, 1000);
+});
