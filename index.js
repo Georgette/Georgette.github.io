2064794ebe56746586d430237bbb5a5eeda932f7
Georgette Pincin <georgette@pincin.com>
2015-08-17 23:42:21 -0400

    improving bio

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 
 $( document ).ready(function() {
-//     var rect = Snap().rect(0, 0, 10, 10);
-// Snap.animate(0, 10, function (val) {
-//     rect.attr({
-//         x: val
-//     });
-// }, 1000);
-// // in given context is equivalent to
-// rect.animate({x: 10}, 1000);
     var $homepage = $('#home-page')
     $('#titles').on('click', function() {
         $homepage.animate({'margin-left':'-100%'}, 1000)
