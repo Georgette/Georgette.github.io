8e3d655f2bf68379c730b06a732bc77954e9c822
Georgette Pincin <georgette@pincin.com>
2015-09-21 15:49:48 -0400

    removed unused js

--- app/main.js
+++ app/main.js
@@ -1,10 +1,2 @@
 require('./styles/main.less')
 var $ = require('jquery')
-
-
-$( document ).ready(function() {
-    var $homepage = $('#home-page')
-    $('#titles').on('click', function() {
-        $homepage.animate({'margin-left':'-100%'}, 1000)
-    })
-});
